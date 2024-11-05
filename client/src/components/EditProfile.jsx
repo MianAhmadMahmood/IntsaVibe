a4f922c149ba788a00927b5fb2a1fa82907768ae
Mian Ahmad Mahmood <157634770+MianAhmadMahmood@users.noreply.github.com>
2024-11-05 10:31:28 +0000

    Update EditProfile.jsx

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -46,7 +46,7 @@ const selectChangeHandler = (value) => {
 
        try {
         setLoading(true);
-        const res = await axios.post(`http://localhost:8000/api/v1/user/profile/edit`,formData,{
+        const res = await axios.post(`https://intsavibe.onrender.com/api/v1/user/profile/edit`,formData,{
             headers:{
                  'Content-Type':'multipart/form-data'
             },
